61947d7cf0d123ff72683d97c4d2db70dc88cf0b
IceScout26 <dhimasscout@gmail.com>
2023-09-28 20:14:04 +0700

    feat : circumference formula at square_formula.js

--- soal1.js
+++ soal1.js
@@ -3,4 +3,6 @@ const fs = require('fs');
 //local module
 const calc = require('./square_formula.js');
 
+//output
 console.log(`Luas dari persegi tersebut adalah ${calc.area(10, 20)}`);
+console.log(`Keliling dari persegi tersebut adalah ${calc.circumference(10, 20)}`);
--- square_formula.js
+++ square_formula.js
@@ -1,3 +1,7 @@
 exports.area = function(width, height) {
     return width * height;
 }
+
+exports.circumference = function(width, height) {
+    return 2 * (width + height);
+}
